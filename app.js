cac2909e7c3f32f20de2e2ec23fe56e9dec694b4
fairyinabottle4 <keithlow98@gmail.com>
2021-06-28 12:09:44 +0800

    11.9 ver 7

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000; //eslint-disable-line no-undef
+console.log(PORT) //eslint-disable-line no-console
 
 app.use(express.static("dist"));
 
